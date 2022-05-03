721ec29b2c01717680abc0be25f8612e0a361c0d
sadko4u <sadko4u@gmail.com>
2022-05-03 23:34:34 +0300

    Version up

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-dyna-processor
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Dynamic Processor Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.4
+ARTIFACT_VERSION            = 1.0.5-devel
 
 
 
