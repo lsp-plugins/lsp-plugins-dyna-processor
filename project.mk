2632fa4c21a9d0588402447bb32479e83c5a3074
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:48:17 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-dyna-processor
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Dynamic Processor Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.9-devel
 
 
